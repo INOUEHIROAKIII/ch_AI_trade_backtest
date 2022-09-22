85dcc81a1eb8e52bcc018fa1e78ad98171211000
hiroaki003 <inoue.hiroaki003@gmail.com>
2022-09-22 23:41:26 +0900

    git 確認

--- opt/debug.py
+++ opt/debug.py
@@ -67,7 +67,7 @@ debug = debug_matplot.debug_plot()
 # csv.get_candlestick_and_create_csv(df_a,"急騰落検知結果10")
 # # ------------------------------------------------------------------------------
 
-print(1111)
+print(111122)
 
 ''' 今回使わない
 # # チャネル引く、matplotlib出力
